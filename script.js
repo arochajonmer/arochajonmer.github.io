e50b4da6b148edaa2c6833f423de760bc72d2507
arocha jonmer <arochajonmer@gmail.com>
2023-02-09 22:43:09 -0300

    buton.onclick-js,footer,body

---
+++ script.js
@@ -0,0 +1,9 @@
+function botonPerfil(){
+    document.querySelector(".boton").style.display='none';
+    document.querySelector(".perfiljs").style.display='block';
+}
+function botonReverso(){
+    document.querySelector(".perfiljs").style.display='none';
+    document.querySelector(".boton").style.display='block';
+    
+}
