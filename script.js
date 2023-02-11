767e4d3340608127203cb614b2ff4def5bf04da8
arocha jonmer <arochajonmer@gmail.com>
2023-02-10 22:57:49 -0300

    terminando botones y estilos de la pagina

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function botonReverso(){
     document.querySelector(".boton").style.display='block';
     
 }
+function botonEstudios(){
+    document.querySelector(".boton2").style.display='none';
+    document.querySelector(".estudiosjs").style.display='block';
+}
+function botonReverso2(){
+    document.querySelector(".estudiosjs").style.display='none';
+    document.querySelector(".boton2").style.display='block';
+    
+}
+function botonExperiencia(){
+    document.querySelector(".boton3").style.display='none';
+    document.querySelector(".experienciajs").style.display='block';
+}
+function botonReverso3(){
+    document.querySelector(".experienciajs").style.display='none';
+    document.querySelector(".boton3").style.display='block';
+    
+}
